a49c428622358113fd77374ff461c2ebac24b927
Samuel Tseng <La6AMDHome@Gmail.com>
2019-02-11 20:41:28 -0800

    Complete Motor Control Implemented

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,14 +1,31 @@
+short i = 205;
+
 void setup() {
+  Serial.begin(9600);
+  Serial.println("~~~Starting Motor~~~");
   pinMode(9, OUTPUT);
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
   TCCR1B = _BV(WGM12) | _BV(CS11) | _BV(CS10);
-  OCR1A = 1023;
+
+  // No Load Operating Values
+  // Start-Up Min 205, Absolute Min: 0
+  // Operating Range ~180 - 1023 (Lowest operating value may be lower)
+  OCR1A = 0;
+  
 }
 
 void loop() {
-  
-  for(int i = 1; i < 1023; i++) {
+
+  // Slowly Rev Motor Up
+  for(; i < 500; i+= 5) {
     OCR1A = i; // Range 0 - 1023
-    delay(5);
+    Serial.println(i);
+    delay(500);
+  }
+
+  // Quickly Decelerate Motor
+  for(; i > 180; i--) {
+    OCR1A = i; // Range 0 - 1023
+    delay(10);
   }
 }
