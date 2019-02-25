c95d6b4cbdf4a16803f199bcffb04f30642777e1
Samuel Tseng <amdtseng@ucdavis.edu>
2019-02-25 13:50:22 -0800

    Added preliminary code for HW INT1

--- controller/controller.ino
+++ controller/controller.ino
@@ -5,6 +5,10 @@ void setup() {
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
   TCCR1B = _BV(WGM12) | _BV(CS11) | _BV(CS10);
 
+  // Enable HW Interrupts: INT1 Rising Interrupt
+  EIMSK = _BV(INT1);
+  EICRA = _BV(ISC11) | _BV(ISC10);
+
   // No Load Operating Values
   // Start-Up Min 205, Absolute Min: 0
   // Operating Range ~180 - 1023 (Lowest operating value may be lower)
@@ -12,19 +16,48 @@ void setup() {
   
 }
 
+ISR(EXT_INT1_vect) {
+  // Perform task here
+}
+
 void loop() {
-  static short i = 205;
+  static char data[6];
+  static char counter = 0;
+  
+  if(Serial.available()>0)
+   {     
+      data[counter] = Serial.read(); // reading the data received from the bluetooth module
 
-  // Slowly Rev Motor Up
-  for(; i < 500; i+= 5) {
-    OCR1A = i; // Range 0 - 1023
-    Serial.println(i);
-    delay(500);
-  }
+      if(data[counter] == '\n'){
+        data[counter] = '\0';
+        counter = 0;
+        
+        int num = atoi(data);
+        
+        if(num > 1023){
+          num = num % 1024;
+        }
+        
+        if(num > 180){
+          digitalWrite(LED_BUILTIN, HIGH);
+          OCR1A = num;
+        } else {
+          digitalWrite(LED_BUILTIN, LOW);
+          OCR1A = 0;
+        }
 
-  // Quickly Decelerate Motor
-  for(; i > 180; i--) {
-    OCR1A = i; // Range 0 - 1023
-    delay(10);
-  }
+        char p[6];
+        itoa(num, p, 10);
+
+        for(char i = 0; data[i] != '\0' && i < 6; i++) {
+          Serial.print(p[i]);
+        }
+        Serial.println("");
+        
+      } else {
+        counter++;
+      }
+   }
+   
+   delay(50);
 }
