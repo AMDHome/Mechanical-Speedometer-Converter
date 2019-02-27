14a78082c4a48c7948ad20259cc7aa3ab6d89800
Samuel Tseng <La6AMDHome@Gmail.com>
2019-02-27 15:51:48 -0800

    added speed sensor code into code. (needs testing)

--- controller/controller.ino
+++ controller/controller.ino
@@ -20,10 +20,19 @@ void updateInputRatio(char numMag, float wheelCirc, float finalDrive) {
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("~~~Starting Motor~~~");
   pinMode(9, OUTPUT);
+
+  // change hardCoded numbers to be read in from EEPROM
+  updateInputRatio(4, 1.04829, 1.0);
+  outRatio = 1.4 * 10000000;
+
+  // Configure PWM (Count Up, Fast PWM 10-bit, CLK/64)
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
   TCCR1B = _BV(WGM12) | _BV(CS11) | _BV(CS10);
+
+  // Enable HW Interrupts: INT1 Rising Interrupt
+  EIMSK = _BV(INT1);
+  EICRA = _BV(ISC11) | _BV(ISC10);
 
   // No Load Operating Values
   // Start-Up Min 205, Absolute Min: 0
@@ -41,19 +50,35 @@ ISR(EXT_INT1_vect) {
   if(ctime < comp_ptime)
     return;
 
-  SPHr = inRatio / (ctime - comp_ptime);
-  targetRPM = (long) SPHr * outRatio / 10000000;
+  SPHr = (unsigned long)inRatio / (ctime - comp_ptime);
+  targetRPM = (unsigned long) SPHr * outRatio / 10000000;
 }
 
 void loop() {
-  static short i = 205;
+  static char data[6];
+  static char counter = 0;
+  
+  if(Serial.available()>0)
+   {     
+      data[counter] = Serial.read(); // reading the data received from the bluetooth module
 
-  // Slowly Rev Motor Up
-  for(; i < 500; i+= 5) {
-    OCR1A = i; // Range 0 - 1023
-    Serial.println(i);
-    delay(500);
-  }
+      if(data[counter] == '\n'){
+        data[counter] = '\0';
+        counter = 0;
+        
+        int num = atoi(data);
+        
+        if(num > 1023){
+          num = num % 1024;
+        }
+        
+        if(num > 180){
+          digitalWrite(LED_BUILTIN, HIGH);
+          OCR1A = num;
+        } else {
+          digitalWrite(LED_BUILTIN, LOW);
+          OCR1A = 0;
+        }
 
         char p[6];
         itoa(num, p, 10);
