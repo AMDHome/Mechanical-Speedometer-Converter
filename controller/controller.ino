d1e3f43ec8f52aa3ebb9a5ad0d1bef07874a2a97
Samuel Tseng <amdtseng@ucdavis.edu>
2019-03-06 14:38:02 -0800

    Added reading in settings format

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,8 +1,11 @@
+#include <EEPROM.h>
+
 volatile unsigned short SPHr = 0;       // Speed Per Hour * 10 (Unit friendly, can use both mph and kph)
 volatile unsigned short targetRPM = 0;  // RPM * 10 (ex. 543.5RPM will be stored at 5435)
 
 unsigned long inRatio;    // input ratio, Also happens to be dt for 0.1 SPH [dt > inRatio => SPHr = 0]
 unsigned long outRatio;   // output ratio * 10,000,000 (to compensate for float)
+unsigned short maxSpeed;  // max speed our speedometer can show
 
 volatile unsigned long pTime = 0;
 
@@ -25,6 +28,7 @@ void setup() {
   // change hardCoded numbers to be read in from EEPROM
   updateInputRatio(4, 1.04829, 1.0);
   outRatio = 1.4 * 10000000;
+  maxSpeed = 160;
 
   // Configure PWM (Count Up, Fast PWM 10-bit, CLK/64)
   TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11) | _BV(WGM10);
@@ -81,47 +85,84 @@ ISR(ANALOG_COMP_vect) {
 }
 
 void loop() {
-  static char data[6];
-  static char counter = 0;
-  
-  if(Serial.available()>0)
-   {     
-      data[counter] = Serial.read(); // reading the data received from the bluetooth module
 
-      if(data[counter] == '\n'){
-        data[counter] = '\0';
-        counter = 0;
-        
-        int num = atoi(data);
-        
-        if(num > 1023){
-          num = num % 1024;
-        }
-        
-        if(num > 180){
-          digitalWrite(LED_BUILTIN, HIGH);
-          OCR1A = num;
-        } else {
-          digitalWrite(LED_BUILTIN, LOW);
-          OCR1A = 0;
-        }
+  checkBT();
 
-        char p[6];
-        itoa(num, p, 10);
-
-        for(char i = 0; data[i] != '\0' && i < 6; i++) {
-          Serial.print(p[i]);
-        }
-        Serial.println("");
-        
-      } else {
-        counter++;
-      }
-   }
-
-   delay(50);
-
-   if(micros() - pTime > inRatio) {
+  if(micros() - pTime > inRatio) {
     SPHr = 0;
-   }
+    targetRPM = 0;
+  }
+}
+
+void checkBT() {
+  static char data[13];
+
+  // check and recieve data
+  switch(recvData(data)) {
+    case 'U': // Store Units
+      EEPROM.update(0, data[0] - '0');
+      break;
+
+    case 'M': // Store Max Speed
+      maxSpeed = (short) atoi(data);
+      EEPROM.put(1, maxSpeed);
+      break;
+      
+    case 'N': // Store Number of Magnets
+      EEPROM.update(3, data[0] - '0');
+      // Update inRatio
+      break;
+
+    case 'F': // Store Final Drive Ratio
+      EEPROM.put(4, ((float) atoi(data)) / 1000000);
+      // Update inRatio
+      break;
+
+    case 'S': // Store Speedometer Ratio
+      outRatio = ((float) atoi(data)) / 1000000;
+      EEPROM.put(8, outRatio);
+      break;
+
+    case 'W': // Store Wheel Circumference
+      EEPROM.put(12, ((float) atoi(data)) / 1000000);
+      // Update inRatio
+      break;
+
+    case '\0':
+    default:
+      break;
+  }
+}
+
+char recvData(char* data) {
+  char type = '\0';
+  byte itr = 0;
+
+  if(Serial.available() > 5) {
+    type = Serial.read();
+
+    if(!isupper(type)) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      return '\0';
+    }
+
+    data[0] = Serial.read();
+
+    if(data[0] != ':') {
+      digitalWrite(LED_BUILTIN, HIGH);
+      return '\0';
+    }
+
+    for(; itr < 13 && (Serial.available() > 0); itr++) {
+      data[itr] = Serial.read();
+    }
+
+    if(itr == 13 && data[10] != '\0') {
+      digitalWrite(LED_BUILTIN, HIGH);
+      return '\0';
+    }
+  }
+
+  digitalWrite(LED_BUILTIN, LOW);
+  return type;
 }
