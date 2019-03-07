1e1dd6106319016b1bdfd64adab31d3d2b432852
jdtorres <jdtorres@ucdavis.edu>
2019-03-06 22:20:36 -0800

    Added some documentation

--- pid.py
+++ pid.py
@@ -5,21 +5,23 @@ import math
 import string
 import pdb
 
-pwm = 180
-current = 2000
-target = 3000
+pwm = 180 # Could be anything 0-1023
+current = 2000 # Could be anything 0-12000
+target = 3000 # Could be anything 0-12000
 max = 1023
 min = 0
 oldErr = 0
 pid_i = 0
-kp = .3
-ki = 0.05
-kd = .2
+kp = .3 # Proportional coefficient
+ki = 0.05 # Integral coefficient 
+kd = .2 # Derivative coefficient 
 elapsedTime = 100
 acceptableErr = 1
 currRPM = []
 pwmRec = []
 rpmToPwm = 10
+# Not a true simulation obviously, but close in spirit. Here pwm is tied in a predictable way to rpm and one could simply
+# deduce the correct pwm using (target/current)*startingPWM = correctPWM. But this is not the way of the real world. 
 print("Starting RPM: " + str(current) + "   Target RPM: " + str(target) + "    Starting PWM: " + str(pwm) + "\n") 
 print("Current Motor RPM    Recommended PWM")
 while abs(target - current) > acceptableErr:
