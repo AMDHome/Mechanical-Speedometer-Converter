c68d9fc370ca5713d03544dc247a7b3c47f673d6
jdtorres <jdtorres@ucdavis.edu>
2019-04-30 14:22:57 -0700

    Fixed some double-versus-int caused erros, GPS now works!

--- SpeedFromGPS/app/src/main/java/com/example/speedfromgps/MainActivity.java
+++ SpeedFromGPS/app/src/main/java/com/example/speedfromgps/MainActivity.java
@@ -1,8 +1,8 @@
 /*
  *  This program uses the android location API to determine the GPS-based speed of the mobile device it runs on. Specifically,
  *  it makes time, latitude, and longitude requests from android.location. The latter two are used in the haversine formula
- *  to calculate distance between two locations. In turn, this distance is divided by the time difference of the locations to
- *  derive speed. While the API also features a getSpeed() function, the consensus seems to be that this function is fairly
+ *  to calculate distance between two locations. Next, this distance is divided by the time difference of the location objects
+ *  to derive speed. While the API also features a getSpeed() function, the consensus seems to be that this function is fairly
  *  unreliable: if it cannot determine the speed, it simply returns 0, a value that must be ignored to avoid bad output.
  *  Requesting time, latitude, and longitude and then calculating the speed manually seems to be the better choice. The
  *  primary documentation I used can be found here: https://developer.android.com/guide/topics/location/strategies.html.
@@ -23,7 +23,7 @@ import android.support.v4.app.ActivityCompat;
 
 public class MainActivity extends Activity {
     // Radius of the Earth in meters, used in calcHaversineDist
-    public static final int EARTH_RADIUS = 6371071;
+    public static final double EARTH_RADIUS = 6371071.0;
     // Creates a reference to the system Location Manager
     LocationManager locationManager;
     // Creates a listener that responds to location updates
@@ -44,9 +44,10 @@ public class MainActivity extends Activity {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
             ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
         }
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // minTime: 0 and minDistance: 0 indicates that the provider should make updates as fast as possible. This seems to be about once per second.
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
+        }
     }
 
     public void useNewLocation(Location location) {
@@ -58,22 +59,23 @@ public class MainActivity extends Activity {
         double newLat = location.getLatitude(); // In degrees
         double newLon = location.getLongitude(); // In degrees
         long currTime = location.getTime(); // Milliseconds since 1/1/70
-        long distance = calcHaversineDist(oldLocation.getLatitude(), oldLocation.getLongitude(), newLat, newLon);
-        int mph = (int) (((distance/1609)/(currTime - oldLocation.getTime())) / 3600000); // Convert meters to miles and ms to hours
+        double distance = calcHaversineDist(oldLocation.getLatitude(), oldLocation.getLongitude(), newLat, newLon);
+        int mph = (int) ((distance/1609.0)/(((double)(currTime - oldLocation.getTime())) / 3600000.0)); // Convert meters to miles and ms to hours
         TextView textView = findViewById(R.id.txtCurrentSpeed);
         textView.setText(mph + " miles/hour");
         oldLocation = location;
     }
+
     // Haversine Formula for distance b/t pts on sphere given lat. & lon.; see https://en.wikipedia.org/wiki/Haversine_formula
-    public static long calcHaversineDist(double lat1, double lon1, double lat2, double lon2) {
+    public static double calcHaversineDist(double lat1, double lon1, double lat2, double lon2) {
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+        double a = Math.sin(dLat / 2.0) * Math.sin(dLat / 2.0)
                 + Math.cos(Math.toRadians(lat1))
-                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
-                * Math.sin(dLon / 2);
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2.0)
+                * Math.sin(dLon / 2.0);
         double c = 2 * Math.asin(Math.sqrt(a));
-        long distInMeters = Math.round(EARTH_RADIUS * c);
+        double distInMeters = EARTH_RADIUS * c;
         return distInMeters;
     }
 
@@ -87,7 +89,7 @@ public class MainActivity extends Activity {
                 useNewLocation(location);
             }
         }
-        // Following functions not used
+        // Following functions not used but required in class which implements LocationListener
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {}
         @Override
