fe1736659d357dacfe398123e609b0cc46944daf
Samuel Tseng <La6AMDHome@Gmail.com>
2019-06-13 17:09:11 -0700

    Added Reset File

---
+++ reset/reset.ino
@@ -0,0 +1,29 @@
+#include <EEPROM.h>
+void setup() {
+  byte units = 1;
+  short maxSpeed = 60;
+  byte nMag = 2;
+  float finalDrive = 1.0;
+  long speedoRatio = 18000000;
+  long wheel = 1263341;
+  
+  Serial.begin(9600);
+  
+  EEPROM.update(0, units);
+  EEPROM.put(1, maxSpeed);
+  EEPROM.update(3, nMag);
+  EEPROM.put(4, finalDrive);
+  EEPROM.put(8, speedoRatio);
+  EEPROM.put(12, wheel);
+
+  char wheelSize[11] = "P205/65R15";
+
+  for(byte i = 0; i < 11; i++) {
+      EEPROM.update(16 + i, wheelSize[i]);
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
